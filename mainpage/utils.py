126be39e1fb0458b5ccbda7050b0fb7a464079a3
igrekoni <igrekoni@gmail.com>
2017-03-22 20:20:50 +0300

    add a lot of cool stuff. need to change svg color

---
+++ mainpage/utils.py
@@ -0,0 +1,18 @@
+from datetime import datetime, date
+from math import fabs
+
+
+def counterHelper():
+    currentYear = datetime.today().year
+    currentMonth = datetime.today().month
+    currentDay = datetime.today().day
+
+    zeroYear = 2015
+    zeroMonth = 3
+    zeroDay = 3
+
+    targetYear = int(currentYear) - zeroYear
+    targetMonth = int(fabs(int(currentMonth) - zeroMonth))
+    targetDay = int(fabs(int(currentDay) - zeroDay))
+
+    return targetYear, targetMonth, targetDay
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from mainpage import utils
+
 
 def mainpage(request):
-
-    return render(request, "base.html", {})
+    delta = utils.counterHelper()
+    newTime = delta[0]
+    newMonth = delta[1]
+    newDay = delta[2]
+    return render(request, "base.html", {'newTime': newTime, 'newMonth': newMonth, 'newDay': newDay})
 
 
 def contacts(request):
