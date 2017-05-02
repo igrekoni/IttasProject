c05c10e09156af1795123caadec36f2e250e6760
igrekoni <igrekoni@gmail.com>
2017-05-02 20:04:59 +0300

    software development page

--- news/models.py
+++ news/models.py
@@ -2,17 +2,23 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
+from django.conf import settings
+from django.utils import timezone
 from django.utils.text import slugify
 from unidecode import unidecode
 
 
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
+
 def upload_location(instance, filename):
-    # filebase, extension = filename.split(".")
-    # return "%s/%s.%s" % (instance.id, instance.id, extension)
     return "%s/%s" % (instance.id, filename)
 
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
@@ -22,8 +28,11 @@ class Post(models.Model):
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    objects = PostManager()
 
     def __unicode__(self):
         return self.title
--- news/views.py
+++ news/views.py
@@ -1,16 +1,22 @@
 from __future__ import unicode_literals
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils import timezone
+
 from news.models import Post
 from .forms import PostForm
 
 
 def news_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Запись успешно добавлена")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -22,6 +28,9 @@ def news_create(request):
 
 def news_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     context = {
         "title": instance.title,
         "instance": instance,
@@ -30,8 +39,19 @@ def news_detail(request, slug=None):
 
 
 def news_list(request):
-    queryset_list = Post.objects.all() #.order_by("-timestamp")
-    paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
+    queryset_list = Post.objects.active()
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+
+    query = request.GET.get("search")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()
+    paginator = Paginator(queryset_list, 5)
     page_request_var = 'page'
     page = request.GET.get(page_request_var)
     try:
@@ -50,6 +70,8 @@ def news_list(request):
 
 
 def news_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -66,8 +88,9 @@ def news_update(request, slug=None):
 
 
 def news_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Запись удалена")
     return redirect("news:list")
-
