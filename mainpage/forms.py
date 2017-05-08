5c268dcd44e81d3a1b523c91fac3f0b76a92acb0
igrekoni <igrekoni@gmail.com>
2017-05-08 13:29:22 +0300

    form labels

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -3,6 +3,17 @@ from .models import Feedback
 
 
 class FeedbackForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(FeedbackForm, self).__init__(*args, **kwargs)
+        self.fields['customer_name'].label = ''
+        self.fields['email'].label = ''
+        self.fields['form_message'].label = ''
+
     class Meta:
         model = Feedback
         exclude = []
+        widgets = {
+            'customer_name': forms.TextInput(attrs={"placeholder": "Ваше имя"}),
+            'email': forms.TextInput(attrs={"placeholder": "E-mail"}),
+            'form_message': forms.TextInput(attrs={"placeholder": "Сообщение"}),
+        }
